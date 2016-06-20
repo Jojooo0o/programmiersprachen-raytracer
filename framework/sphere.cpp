3dec113c899f7108c6e0c09d421d693be9cc3767
Jojooo0o <jonasdorsch@web.de>
2016-06-20 12:07:01 +0200

    comment sphere

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -1,18 +1,21 @@
 #include "sphere.hpp"
 
-Sphere::Sphere():
+Sphere::Sphere()://Constructor with console output
 	Shape(std::string("Sphere"), Color{0.0f, 0.0f, 0.0f}),
 	center_(glm::vec3{1.0f, 1.0f, 1.0f}),
 	rad_(1.0f)
 	{ std::cout << "Sphere::Constructor" << std::endl; }
 
+//Custom constructor with console output
 Sphere::Sphere(std::string const& name, Color const& color, glm::vec3 const& center, float rad):
 	Shape(name, color),
 	center_(center),
 	rad_(rad){ std::cout << "Sphere::Constructor" << std::endl; }
 
+//Desctructor
 Sphere::~Sphere() { std::cout << "Sphere::Destructor" << std::endl; }
 
+//getter
 glm::vec3 const& Sphere::get_center() const{
 	return center_;
 }
@@ -20,22 +23,23 @@ glm::vec3 const& Sphere::get_center() const{
 float Sphere::get_radius() const{
 	return rad_;
 }
-
+//oberflaeche
 float Sphere::area() const {
 	return rad_*rad_*M_PI*4;
 }
-
+//volumen
 float Sphere::volume() const {
 	return (4/3)*M_PI*rad_*rad_*rad_;
 }
-
+//Printmethode
 std::ostream& Sphere::print(std::ostream& os) const{
 	Shape::print(os);
 	os << "Center: " << center_.x << ", " << center_.y << ", " << center_.z << std::endl;
 	os << "Radius: " << rad_ << std::endl;
 	return os;
 }
-
+//gibt true false aus, wenn strahl kugel schneidet oder nicht
+//setzt float auf abstand zwischen strahlstartpunkt u kugel
 bool Sphere::intersect (Ray const& ray, float& distance){
 	auto v = glm::normalize(ray.direction);
 	return glm::intersectRaySphere(ray.origin, v, center_, rad_*rad_, distance);
